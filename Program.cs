fca36367f5d995e865edd0537f914b435273b03e
Kirk37492920203 <l.kadriu.im26@stud.bbbaden.ch>
2026-08-28 16:52:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Erste Note Informatik");
+    Double note = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Zweite Note Informatik");
+Double note1  = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Dritte Note Informatik");
+Double note2 = Convert.ToDouble(Console.ReadLine());
+Double durchschnitt = (note + note1 + note2) / 3.0;
+Console.WriteLine("Dein Informatik schnitt ist :" + durchschnitt);
+Console.ReadKey();
